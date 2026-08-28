2f80408daecf72bac5b7ff76ddede61b54e37fda
sharon884 <sharonsharon9048461583@gmail.com>
2026-08-28 11:47:38 +0530

    chore: configure environment and health check

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
 import app from "./app.js";
+import { env } from "./ config/env.js";
 
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => {
-    console.log(`Reach backend running on port http://localhost:${PORT}`);
+app.listen(env.PORT, () => {
+  console.log(`Reach backend running on port ${env.PORT}`);
 });
