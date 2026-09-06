c44fd3879c67d73a4595f1474fa1daa577a0dc40
sharon884 <sharonsharon9048461583@gmail.com>
2026-09-06 16:51:47 +0530

    chore(config): configure cors for frontend

--- src/app.ts
+++ src/app.ts
@@ -3,8 +3,19 @@ import { authRouter } from "./presentation/routes/auth/auth.routes.js";
 import { adminRouter } from "./presentation/routes/admin/admin.routes.js";
 import { errorMiddleware } from "./presentation/middlewares/error.middleware.js";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
 const app = express();
+
+
+app.use(
+    cors({
+        origin: "http://localhost:5173",
+        credentials: true,
+    }),
+);
+
+
 
 app.use(express.json());
 app.use(cookieParser());
