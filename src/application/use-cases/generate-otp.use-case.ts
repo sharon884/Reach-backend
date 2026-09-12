56e086ef560267d9f5a861f7424f771510ccdeed
sharon884 <sharonsharon9048461583@gmail.com>
2026-09-12 11:23:53 +0530

    refactor: use Redis OTP store in generation flow

--- src/application/use-cases/generate-otp.use-case.ts
+++ src/application/use-cases/generate-otp.use-case.ts
@@ -1,13 +1,10 @@
-import { randomUUID } from "node:crypto";
-
-import {
-    OtpPurpose,
-    OtpVerification,
-} from "@/domain/entities/otp-verification.entity";
+import type { OtpPurpose } from "@/domain/entities/otp-verification.entity";
 
 import { IGenerateOtpUseCase } from "../abstractions/use-cases/generate-otp.use-case.js";
 
-import { IOtpVerificationRepository } from "@/domain/repositories/otp-verification.repository";
+import { IOtpStore } from "@/application/services/otp-store";
+
+import { OTP_POLICIES } from "@/application/services/otp-policy";
 
 import { IUserRepository } from "@/domain/repositories/user.repository";
 
@@ -23,54 +20,46 @@ import { AUTH_MESSAGES } from "@/shared/constants/messages/auth.messages";
 
 import { StatusCodes } from "http-status-codes";
 
-export class GenerateOtpUseCase implements IGenerateOtpUseCase  {
-    constructor(
-        private readonly _userRepository: IUserRepository,
-        private readonly _otpRepository: IOtpVerificationRepository,
-        private readonly _otpGenerator: IOtpGenerator,
-        private readonly _otpHasher: IOtpHasher,
-        private readonly _emailSender: IEmailSender,
-    ) { }
+export class GenerateOtpUseCase implements IGenerateOtpUseCase {
+  constructor(
+    private readonly _userRepository: IUserRepository,
+    private readonly _otpStore: IOtpStore,
+    private readonly _otpGenerator: IOtpGenerator,
+    private readonly _otpHasher: IOtpHasher,
+    private readonly _emailSender: IEmailSender,
+  ) {}
 
-    async execute(
-        userId: string,
-        purpose: OtpPurpose,
-    ): Promise<string> {
-        const user = await this._userRepository.findById(userId);
+  async execute(
+    userId: string,
+    purpose: OtpPurpose,
+  ): Promise<string> {
+    const user = await this._userRepository.findById(userId);
 
-        if (!user) {
-            throw new AppError(
-                AUTH_MESSAGES.USER_NOT_FOUND,
-                StatusCodes.NOT_FOUND,
-            );
-        }
-        await this._otpRepository.invalidateActiveOtp(userId, purpose);
-
-        const otp = this._otpGenerator.generate();
-
-        const codeHash = await this._otpHasher.hash(otp);
-
-        const otpVerification: OtpVerification = {
-            id: randomUUID(),
-            userId,
-            codeHash,
-            purpose,
-            expiresAt:  new Date(Date.now() + 5 * 60 * 1000),
-            attempts: 0,
-            resendCount: 0,
-            verifiedAt: null,
-            createdAt: new Date(),
-        };
-
-        await this._otpRepository.create(otpVerification);
-
-        console.log(otp)
-
-        await this._emailSender.sendOtp(
-            user.email,
-            otp,
-        );
-
-        return otp;
+    if (!user) {
+      throw new AppError(
+        AUTH_MESSAGES.USER_NOT_FOUND,
+        StatusCodes.NOT_FOUND,
+      );
     }
+
+    const otp = this._otpGenerator.generate();
+
+    const codeHash = await this._otpHasher.hash(otp);
+
+    const policy = OTP_POLICIES[purpose];
+
+    await this._otpStore.save(
+      userId,
+      purpose,
+      codeHash,
+      policy.expiresInSeconds,
+    );
+
+    await this._emailSender.sendOtp(
+      user.email,
+      otp,
+    );
+
+    return otp;
+  }
 }
