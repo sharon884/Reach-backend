bda74ded0771c53d2de0cb7ad3b02f022d15aced
sharon884 <sharonsharon9048461583@gmail.com>
2026-09-13 16:17:24 +0530

    feat: add reset password controller

--- src/shared/constants/messages/auth.messages.ts
+++ src/shared/constants/messages/auth.messages.ts
@@ -1,21 +1,35 @@
 export const AUTH_MESSAGES = {
 
   SIGNUP_SUCCESS: "User registered successfully",
+
   EMAIL_ALREADY_REGISTERED: "Email is already registered",
+
   INVALID_CREDENTIALS: "Invalid email or password",
+
   EMAIL_NOT_VERIFIED: "Email is not verified",
+
   EMAIL_ALREADY_VERIFIED: "Email is already verified",
+
   USER_NOT_FOUND: "User not found",
+
   ACCOUNT_NOT_ACTIVE : "Account not active",
+
   LOGIN_SUCCESS : "Success! Logged in successfully. Welcome back to your community.",
+
   UNAUTHORIZED: "Unauthorized access",
 
   OTP_INVALID: "Invalid OTP",
-  OTP_EXPIRED: "OTP has expired",
-  OTP_REQUIRED: "OTP is required",
-  OTP_VERIFICATION_SUCCESS: "Email verified successfully",
-  OTP_SENT_SUCCESSFULLY: "OTP sent successfully",
-  PASSWORD_RESET_OTP_SENT : "A password reset OTP has been sent to your registered email."
 
+  OTP_EXPIRED: "OTP has expired",
+
+  OTP_REQUIRED: "OTP is required",
+
+  OTP_VERIFICATION_SUCCESS: "Email verified successfully",
+
+  OTP_SENT_SUCCESSFULLY: "OTP sent successfully",
+
+  PASSWORD_RESET_OTP_SENT : "A password reset OTP has been sent to your registered email.",
+
+   PASSWORD_RESET_SUCCESS: "Your password has been successfully reset. You can now log in with your new password.",
   
 } as const;
