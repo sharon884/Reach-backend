34b9ccc3eb55ac7b0642687bf57acb9afe4208b4
sharon884 <sharonsharon9048461583@gmail.com>
2026-09-14 12:33:50 +0530

    feat: add Google authentication controller

--- src/shared/constants/messages/auth.messages.ts
+++ src/shared/constants/messages/auth.messages.ts
@@ -33,6 +33,8 @@ export const AUTH_MESSAGES = {
    PASSWORD_RESET_SUCCESS: "Your password has been successfully reset. You can now log in with your new password.",
 
    INVALID_GOOGLE_TOKEN: "Invalid Google authentication token",
+
+   GOOGLE_LOGIN_SUCCESS: "Successfully logged in with Google.",
   
 } as const;
 
