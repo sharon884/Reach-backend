3e648279f459af632ab0973286ea4a3036a15746
sharon884 <sharonsharon9048461583@gmail.com>
2026-09-15 15:58:53 +0530

    feat: add property option domain entity

---
+++ src/domain/catalog/entities/property-option.entity.ts
@@ -0,0 +1,18 @@
+export interface PropertyOption {
+     
+     id : string;
+
+     propertyId : string;
+      
+     label : string;
+
+     value : string;
+
+     displayOrder : string;
+
+     isActive : boolean;
+
+     createdAt : Date;
+
+     updatedAt : Date;
+}
